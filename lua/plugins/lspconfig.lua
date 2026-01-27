c5c7ce28336ab5481f93704a442cda6197a61ce8
AniketMPat <ampatel@ualberta.ca>
2026-01-26 20:40:13 -0700

    lspconfig and onedark

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -170,6 +170,9 @@ return {
                             vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
                         end, "[T]oggle Inlay [H]ints")
                     end
+
+                    -- Enable inlay hints by default
+                    vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
                 end,
             })
 
@@ -218,7 +221,7 @@ return {
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
+                clangd = {},
                 -- gopls = {},
                 -- pyright = {},
                 -- rust_analyzer = {},
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,26 +1,26 @@
 return {
-	-- {
-	-- 	"navarasu/onedark.nvim",
-	-- 	priority = 1000,
-	--        opts = { style = "cool"},
-	-- 	config = function()
-	-- 		require("onedark").setup({
-	-- 			style = "cool",
-	-- 		})
-	--            require("onedark").load()
-	--        end,
-	-- },
-	{
-	  'folke/tokyonight.nvim',
-	  priority = 1000, -- Make sure to load this before all the other start plugins.
-	  config = function()
-	    ---@diagnostic disable-next-line: missing-fields
-	    require('tokyonight').setup {
-	      styles = {
-	        comments = { italic = false }, -- Disable italics in comments
-	      },
-	    }
-	    vim.cmd.colorscheme 'tokyonight-night'
-	  end,
-	},
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        opts = { style = "cool" },
+        config = function()
+            require("onedark").setup({
+                style = "deep",
+            })
+            require("onedark").load()
+        end,
+    },
+    {
+        "folke/tokyonight.nvim",
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("tokyonight").setup({
+                styles = {
+                    comments = { italic = false }, -- Disable italics in comments
+                },
+            })
+            -- vim.cmd.colorscheme("tokyonight-night")
+        end,
+    },
 }
