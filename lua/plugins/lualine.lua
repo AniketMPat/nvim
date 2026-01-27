0875b3b7f4033c48b04fecb9b46b7bcd0c12cf0a
AniketMPat <ampatel@ualberta.ca>
2026-01-26 17:03:34 -0700

    bufferline and lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,53 @@
+-- return {
+--   'nvim-lualine/lualine.nvim',
+--   dependencies = { 'nvim-tree/nvim-web-devicons' },
+-- }
+-- init.lua (or plugins.lua if you've structured it)
+return {
+    -- ... other plugins ...
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }, -- Optional: for icons
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = true,
+                    theme = 'auto', -- or a specific theme like 'dracula', 'gruvbox', etc.
+                    component_separators = { left = '', right = '' },
+                    section_separators = { left = '', right = '' },
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    always_divide_middle = true,
+                    globalstatus = true,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                    },
+                },
+                sections = {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff', 'diagnostics' },
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'location' },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                tabline = {},
+                winbar = {},
+                extensions = {},
+            }
+        end,
+    },
+    -- ... other plugins ...
+}
